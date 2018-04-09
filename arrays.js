a5614e35b67f03f3e91c3de978e51bdb125c3d43
Brindha S <brinsree@gmail.com>
2018-04-09 22:19:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array1,item){
-  array1.unshift(item);
+  return array1.unshift(item);
   
 }
 function destructiveAddElementToBeginningOfArray(array1,item){
